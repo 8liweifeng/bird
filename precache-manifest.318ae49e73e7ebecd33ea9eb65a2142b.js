fe4b1e52a18e623375bc34e8bb49d6043285c858
Liwei Feng <liweeiif@gmail.com>
2023-02-16 01:54:59 -0700

    Updates

---
+++ precache-manifest.318ae49e73e7ebecd33ea9eb65a2142b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cc52cdeef45f47c9e36f126e1160ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4734bcb388811a65782",
+    "url": "./static/css/main.5f11fe4e.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "./static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4734bcb388811a65782",
+    "url": "./static/js/main.0dfe441f.chunk.js"
+  },
+  {
+    "revision": "239a74ba6fe8e6b6cf40",
+    "url": "./static/js/runtime-main.6aa09aaf.js"
+  }
+]);
